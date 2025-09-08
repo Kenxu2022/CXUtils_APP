a44ee0a30a9f3ba0811d87ef971e0f5eadc831d4
Kenxu2022 <kenxu2022@gmail.com>
2025-09-08 15:37:26 +0800

    use `SafeArea()` to adjust widget position

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -9,7 +9,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('CXUtils'),
       ),
-      body: const Placeholder(),
+      body: const SafeArea(child: Placeholder()),
     );
   }
 }
