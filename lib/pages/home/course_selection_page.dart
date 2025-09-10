bf492d97dabc64e1907d94fdca83d7dd855dfd35
Kenxu2022 <kenxu2022@gmail.com>
2025-09-11 02:23:04 +0800

    allow using `overrideCourseID` and `overrideClassID` in course selection page

--- lib/pages/home/course_selection_page.dart
+++ lib/pages/home/course_selection_page.dart
@@ -1,3 +1,4 @@
+import 'package:cxutils/providers/settings_provider.dart';
 import 'package:cxutils/utils/course_logic.dart';
 import 'package:flutter/material.dart';
 
@@ -14,11 +15,16 @@ class _CourseSelectionPageState extends State<CourseSelectionPage> {
   late Future<Map<String, List<Course>>> _coursesFuture;
   final Map<String, Course> _selectedCourse = {};
   bool _showNextButton = false;
+  late SettingsProvider _settings;
 
   @override
   void initState() {
     super.initState();
-    _coursesFuture = fetchCoursesForUsers(widget.selectedUsernames);
+    _settings = SettingsProvider.instance;
+    if (_settings.overrideCourseID.isEmpty ||
+        _settings.overrideClassID.isEmpty) {
+      _coursesFuture = fetchCoursesForUsers(widget.selectedUsernames);
+    }
   }
 
   void _onCourseSelected(String username, Course? selectedCourse,
@@ -72,79 +78,105 @@ class _CourseSelectionPageState extends State<CourseSelectionPage> {
         title: const Text('选择课程'),
       ),
       body: SafeArea(
-        child: FutureBuilder<Map<String, List<Course>>>(
-          future: _coursesFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Center(child: Text('加载课程失败: ${snapshot.error}'));
-            } else if (snapshot.hasData) {
-              final allCourses = snapshot.data!;
-              return Column(
+        child: _settings.overrideCourseID.isNotEmpty &&
+                _settings.overrideClassID.isNotEmpty
+            ? Center(
+                child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Expanded(
-                    child: ListView.builder(
-                      itemCount: widget.selectedUsernames.length,
-                      itemBuilder: (context, index) {
-                        final username = widget.selectedUsernames[index];
-                        final courses = allCourses[username] ?? [];
-                        return Card(
-                          margin: const EdgeInsets.symmetric(
-                              horizontal: 12, vertical: 6),
-                          child: ListTile(
-                            title: Text(
-                              '用户$username：',
-                              style:
-                                  const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                            ),
-                            subtitle: DropdownButton<Course>(
-                              isExpanded: true,
-                              hint: const Text('--请选择--'),
-                              value: _selectedCourse[username],
-                              onChanged: (Course? newValue) {
-                                _onCourseSelected(
-                                    username, newValue, allCourses);
+                  const Text(
+                    "已手动填入课程信息\n请直接点击下一步按钮",
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      final selectedCourseDetails = [
+                        _settings.overrideCourseID,
+                        _settings.overrideClassID
+                      ];
+                      print("selected users: ${widget.selectedUsernames}");
+                      print("selected course details: $selectedCourseDetails");
+                    },
+                    child: const Text('下一步'),
+                  )
+                ],
+              ))
+            : FutureBuilder<Map<String, List<Course>>>(
+                future: _coursesFuture,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('加载课程失败: ${snapshot.error}'));
+                  } else if (snapshot.hasData) {
+                    final allCourses = snapshot.data!;
+                    return Column(
+                      children: [
+                        Expanded(
+                          child: ListView.builder(
+                            itemCount: widget.selectedUsernames.length,
+                            itemBuilder: (context, index) {
+                              final username = widget.selectedUsernames[index];
+                              final courses = allCourses[username] ?? [];
+                              return Card(
+                                margin: const EdgeInsets.symmetric(
+                                    horizontal: 12, vertical: 6),
+                                child: ListTile(
+                                  title: Text(
+                                    '用户$username：',
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16),
+                                  ),
+                                  subtitle: DropdownButton<Course>(
+                                    isExpanded: true,
+                                    hint: const Text('--请选择--'),
+                                    value: _selectedCourse[username],
+                                    onChanged: (Course? newValue) {
+                                      _onCourseSelected(
+                                          username, newValue, allCourses);
+                                    },
+                                    items: courses
+                                        .map<DropdownMenuItem<Course>>(
+                                            (Course course) {
+                                      return DropdownMenuItem<Course>(
+                                        value: course,
+                                        child: Text(course.name),
+                                      );
+                                    }).toList(),
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                        if (_showNextButton)
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                if (_selectedCourse.isNotEmpty) {
+                                  final firstCourse =
+                                      _selectedCourse.values.first;
+                                  final selectedCourseDetails = [
+                                    firstCourse.courseId,
+                                    firstCourse.classId
+                                  ];
+                                  print("selected users: ${widget.selectedUsernames}");
+                                  print("selected course details: $selectedCourseDetails");
+                                }
                               },
-                              items: courses.map<DropdownMenuItem<Course>>(
-                                  (Course course) {
-                                return DropdownMenuItem<Course>(
-                                  value: course,
-                                  child: Text(course.name),
-                                );
-                              }).toList(),
+                              child: const Text('下一步'),
                             ),
                           ),
-                        );
-                      },
-                    ),
-                  ),
-                  if (_showNextButton)
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          if (_selectedCourse.isNotEmpty) {
-                            final firstCourse = _selectedCourse.values.first;
-                            final selectedCourseDetails = [
-                              firstCourse.courseId,
-                              firstCourse.classId
-                            ];
-                            print("selected users: ${widget.selectedUsernames}");
-                            print(
-                                "selected course details: $selectedCourseDetails");
-                          }
-                        },
-                        child: const Text('下一步'),
-                      ),
-                    ),
-                ],
-              );
-            } else {
-              return const Center(child: Text('没有课程数据'));
-            }
-          },
-        ),
+                      ],
+                    );
+                  } else {
+                    return const Center(child: Text('没有课程数据'));
+                  }
+                },
+              ),
       ),
     );
   }
