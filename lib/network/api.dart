96b6a4bb37673754703d35161b26fac7c8fd3b8c
Kenxu2022 <kenxu2022@gmail.com>
2025-09-11 11:07:55 +0800

    add activity selection api function

--- lib/network/api.dart
+++ lib/network/api.dart
@@ -125,3 +125,54 @@ Future<Map<String, dynamic>> getCourse(
     };
   }
 }
+
+Future<Map<String, dynamic>> getActivity(
+  /* return format:
+  {
+    'success': bool,
+    'data': List<Map<String, dynamic>> | String, // if success is true, data is List<Map<String, dynamic>>, else data is String (error message)
+      format: [
+        {
+          'name': 
+          'startTime': 
+          'endTime':
+          'active': --> Boolean, whether the activity is currently active
+          'activeID': 
+          'activeType':
+        },
+        ...
+      ]
+  }
+  */
+  String username,
+  String courseID,
+  String classID,
+) async {
+  final url = '${settings.endpoint}/getActivity';
+  final headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ${await getToken()}',
+  };
+  final body = jsonEncode({
+    'username': username,
+    'courseID': courseID,
+    'classID': classID,
+  });
+
+  try {
+    final response = await http
+        .post(
+          Uri.parse(url),
+          headers: headers,
+          body: body,
+        )
+        .timeout(const Duration(seconds: 5));
+    final Map<String, dynamic> responseBody = jsonDecode(response.body);
+    return responseBody;
+  } on TimeoutException {
+    return {
+      'success': false,
+      'data': '请求超时，请检查网络',
+    };
+  }
+}
